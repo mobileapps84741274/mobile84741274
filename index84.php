5d1cb1eda624535bcb092e259391a74827c0a601
mobileapps84741274 <64268204+mobileapps84741274@users.noreply.github.com>
2020-09-13 11:45:22 +0000

    Create index84.php

---
+++ index84.php
@@ -0,0 +1,548 @@
+<header>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
+<script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+</header>
+
+<?php
+
+$google8474 = array();
+
+$mobile84 = file_get_contents("https://arionumpool.com/stats.php?q=acc&id=4dkJEWHWMVpTFTD6pZr8111zWoRNJUmoStit56uL3Urhte98L1Lm3VBWhUCkS6MAYyyPFNSVCqbKccLTmFbiYgA6");
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<span class=\"pull-right\">(.*?)<\/span>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;border-color:#039be5;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+$google8474[] = $google84 * 0.026;
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$mobile84 = file_get_contents("https://arionumpool.com/stats.php?q=acc&id=36emHJrD7TjVnbrvJzpooocHD2vnEMZHjHMm3kk3tMpMhPgw5NfYPtZEiu6nBJjeJi21vwLAD2d5afKDRiJPeQKW");
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<span class=\"pull-right\">(.*?)<\/span>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;border-color:#039be5;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+$google8474[] = $google84 * 0.026;
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$mobile84 = file_get_contents("https://arionumpool.com/stats.php?q=acc&id=4migato2bH4mdWUVMWTrUC49bCGARNnqR1YAteUJ3tEJVobjrooSa6NqK2Uwehu9WFtpLwwEmcwT63Xcphun1bwj");
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<span class=\"pull-right\">(.*?)<\/span>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;border-color:#039be5;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+$google8474[] = $google84 * 0.026;
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$mobile84 = file_get_contents("https://arionumpool.com/stats.php?q=acc&id=xZDcVyJeLLxqqsn6CeygQMR5zBTi9Gfk1d7XG3rj7WmypGrqRVBXWHg3WMk7yNfpbXnw85LBzY6j42F4qgpJRUP");
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<span class=\"pull-right\">(.*?)<\/span>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;border-color:#039be5;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+$google8474[] = $google84 * 0.026;
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$mobile84 = file_get_contents("https://arionumpool.com/stats.php?q=acc&id=2z2hwRPjLrQoN3UBsCX4EMRQxdcMxMFsQeJkUnZvgGXZnAsG2dBf3FXZdrk9stxzgd2Hrzdy8HxQb6AGFEEa5xkk");
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<span class=\"pull-right\">(.*?)<\/span>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;border-color:#039be5;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+$google8474[] = $google84 * 0.026;
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$mobile84 = file_get_contents("https://arionumpool.com/stats.php?q=acc&id=28xqj4rxaoBfMeHnCb9gS9t3ezWMvPZ34qYhD4Tu5iSZhEpc3Q6cZYWowgyLuSqsAnbsy2Dw3cNjWR3A6WMnGoa2");
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<span class=\"pull-right\">(.*?)<\/span>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;border-color:#039be5;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+$google8474[] = $google84 * 0.026;
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$mobile84 = file_get_contents("https://arionumpool.com/stats.php?q=acc&id=45rqQs8nm37XEtzK38aSJyDiyXXDKCtkFhbM7WtFGAATBLqAHq1CE9HyZZ7V4wtQ94qZPQbJ89mQgyXcS8XRWUB9");
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<span class=\"pull-right\">(.*?)<\/span>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;border-color:#039be5;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+$google8474[] = $google84 * 0.026;
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$mobile84 = file_get_contents("https://arionumpool.com/stats.php?q=acc&id=2tmy46jHQHTjMEsvgEz3BMMCnVcER97gqenr1zqzhpwd3VaiN1nS6m8eK8WTiNhXyHJBWS1FgZBpy2Ew5xxQ4Tio");
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<span class=\"pull-right\">(.*?)<\/span>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
+
+<?php
+
+$pattern = "/<div class=\"huge\">(.*?)<\/div>/";
+
+if(preg_match_all($pattern, $mobile84, $matches)) {
+
+echo "<div style='color:#888888;border-style:solid;padding:12px;padding-left:34px;position:relative;border-color:#039be5;'>";
+
+$google84 = preg_replace("/<br>(.*)/","",$matches[1][3]);
+
+$google8474[] = $google84 * 0.026;
+
+echo "<i class='material-icons mdc-text-field__icon mdc-text-field__icon--leading' style='position:absolute;padding-top:0px;left:-8px;top:10px;'>attach_money</i>" . $google84 * 0.026;
+
+echo "</div>";
+
+}
+
+?>
+
+<?php
+
+echo "<br>";
+
+?>
