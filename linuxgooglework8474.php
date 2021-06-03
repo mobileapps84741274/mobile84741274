ff24104611a50733f48a738f9871a04d616fbc4c
mobileapps84741274 <64268204+mobileapps84741274@users.noreply.github.com>
2021-06-03 16:14:55 +0200

    Update linuxgooglework8474.php

--- linuxgooglework8474.php
+++ linuxgooglework8474.php
@@ -1,5 +1,5 @@
 <?php
 
-shell_exec("DISPLAY=:82.0 sudo bash /var/www/html/linuxgooglework8474 &> /dev/null &");
+shell_exec("DISPLAY=:82.0 sudo -u root bash /var/www/html/linuxgooglework8474 &> /dev/null &");
 
 ?>
