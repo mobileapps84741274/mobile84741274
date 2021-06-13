e6b616a1930f86376266258fcac163d4c75b36e5
mobileapps84741274 <64268204+mobileapps84741274@users.noreply.github.com>
2021-06-13 15:34:02 +0200

    Update linuxclick84.php

--- linuxclick84.php
+++ linuxclick84.php
@@ -1,5 +1,7 @@
 <?php
 
-shell_exec("DISPLAY=:82.0 sudo -u root bash /var/www/html/linuxclick84 &> /dev/null &");
+$linux8474 = $_GET['linux84'];
+
+shell_exec("echo $linux8474 >> ./linuxclick8474;DISPLAY=:82.0 sudo -u root bash /var/www/html/linuxclick84 &> /dev/null &");
 
 ?>
